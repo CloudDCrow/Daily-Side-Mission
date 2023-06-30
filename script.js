184166cb72397faf8ecc980cdaff493f50ae072c
CloudDCrow <roki.robic88@gmail.com>
2023-07-01 00:09:46 +0200

    Made it so missions change every day, only one mission per day

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const xpNum = document.querySelector('#xp-num');
 const completedButton = document.querySelector('#completed-button');
 const attemptedButton = document.querySelector("#attempted-button");
 
-const date = new Date();
-const currentDate = date.getDate();
+const currentDate = new Date();
+
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
+
+const dateString = `${day}/${month}/${year}`;
 
 let xp = 0;
 let level = 1;
@@ -20,6 +25,7 @@ const getRandomMission = () => {
       const randomIndex = Math.floor(Math.random() * missions.missions.length);
       const randomMission = missions.missions[randomIndex];
       theMission.textContent = randomMission;
+      localStorage.setItem('currentMission', randomMission);
     });
 }
 
@@ -35,16 +41,27 @@ function updateXP() {
   }
 }
 
+function checkDate() {
+  if (localStorage.getItem('date') === null) {
+    getRandomMission();
+    localStorage.setItem('date', dateString);
+  }
+  if (localStorage.getItem('date') === dateString) {
+    theMission.textContent = localStorage.getItem('currentMission');
+  }
+  if (localStorage.getItem('date') !== dateString) {
+    getRandomMission();
+    localStorage.setItem('date', dateString);
+    localStorage.setItem('completed', "False");
+  }
+}
+
 function updateXPBar() {
   const progress = xp / xpRequirement * 100;
   xpProgress.style.width = `${progress}%`;
   xpNum.textContent = `${xp}/${xpRequirement}`;
   levelNum.textContent = level;
 }
-  
-getRandomMission();
-updateXP();
-updateXPBar();
 
 function levelUp() {
   level++;
@@ -69,8 +86,7 @@ function handleCompletedButtonClick() {
   
   localStorage.setItem('xp', xp);
   updateXPBar();
-  completedButton.style.display = 'none';
-  attemptedButton.style.display = 'none';
+  missionDone();
 }
 
 function handleAttemptedButtonClick() {
@@ -84,9 +100,27 @@ function handleAttemptedButtonClick() {
 
   localStorage.setItem('xp', xp)
   updateXPBar();
+  missionDone();
+}
+
+function missionDone() {
   completedButton.style.display = 'none';
   attemptedButton.style.display = 'none';
+  theMission.textContent = "Good Job";
+  localStorage.setItem('completed', 'True');
 }
+
+function checkMissionCompletion() {
+  if (localStorage.getItem('completed') == 'True' & 
+      localStorage.getItem('date') == dateString) {
+    missionDone();
+  }
+}
+
+checkDate();
+updateXP();
+updateXPBar();
+checkMissionCompletion();
 
 completedButton.addEventListener('click', handleCompletedButtonClick);
 attemptedButton.addEventListener('click', handleAttemptedButtonClick);
